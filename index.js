1568d0006b1031a82fca2404f77a9202a32dcf1d
BathriBBN <bbnbadhri@gmail.com>
2023-05-08 21:32:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const express = require("express");
+
+/// time stamp
+
+let time = Date.now();
+console.log(time);
+let date = new Date();
+let utc = date.toUTCString();
+console.log("date", date);
+console.log("utc", utc);
